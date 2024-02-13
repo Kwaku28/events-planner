8112fe8866718bf4973237cadcda3a872f5849ff
Lawrence Amoafo <rekenburg777@gmail.com>
2024-02-13 19:29:28 +0000

    Pre-render all events using `getStaticProps` function

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/event-search";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import { useRouter } from "next/router";
 
-function EventsPage() {
-  const events = getAllEvents();
+function EventsPage(props) {
+  const { events } = props;
   const router = useRouter();
 
   function findEventsHandler(year, month) {
@@ -19,6 +19,17 @@ function EventsPage() {
       <EventList events={events} />
     </Fragment>
   )
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 60
+  }
 }
 
 export default EventsPage;
